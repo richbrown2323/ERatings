fdf80526baf293fe7dba98bfae1786783ca5d68a
richbrown2323 <rich@westcoastanalytics.com>
2026-05-12 13:38:30 -0500

    Update PairRatingsID.py

--- PairRatingsID.py
+++ PairRatingsID.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 
 APP_DIR = Path(__file__).parent
-INPUT_FILE = APP_DIR / "ErateNCESmpnet_best_match_for_each_A_WA.csv"
+INPUT_FILE = APP_DIR / "ErateNCESmpnet_best_match_for_each_A_ID_LT95.csv"
 
 df = pd.read_csv(INPUT_FILE)
 
